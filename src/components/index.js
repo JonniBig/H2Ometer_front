e8da07d8a08976ea86847968217833a6d7aac83b
JonniBig <bigun.i88@gmail.com>
2024-01-27 15:03:32 +0200

    routes&calendar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Calendar } from './Calendar/Calendar';
 export { default as Header } from './Header/Header';
 export { default as Layout } from './Layout/Layout';
+export { default as RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
+export { default as PrivateRoute } from './PrivateRoute/PrivateRoute';
