5fd3f43db5571856f408bcca31c76badd48d4b3e
Veronika <nikakras.dance@gmail.com>
2024-01-28 21:26:32 +0200

    add some new

---
+++ src/components/Logout/Logout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyledLogout } from './Logout.styled';
+
+const Logout = () => {
+  return (
+    <div className="backdrop isHidden">
+      <StyledLogout>
+        <div className="logoutTitle">
+          <h4 className="logoutTitle">Log out</h4>
+          <button type="button" className="closeModal"></button>
+          <p className="logoutQuest">Do you really want to leave?</p>
+        </div>
+        <div className="logoutBtns">
+          <button type="button" className="btnCan">
+            Cancel
+          </button>
+          <button type="button" className="btnLog">
+            Log out
+          </button>
+        </div>
+      </StyledLogout>
+    </div>
+  );
+};
+
+export default Logout;
---
+++ src/components/Logout/Logout.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledLogout = styled.div`
+  backdrop {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: ;
+    height: ;
+    border-radius: 10px;
+
+    background: #fff;
+    overflow: hidden;
+    overflow-y: auto;
+  }
+  isHidden {
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: none;
+  }
+`;
