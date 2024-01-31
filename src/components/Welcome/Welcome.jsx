a3ad2a10379af30a61b329d189a626d8bbd099f9
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2024-01-31 19:02:46 +0100

    header

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,7 @@
 import { WaterConsumptionTracker } from './WaterConsumptionTracer/WaterСonsumptionTracker.jsx';
-import { Conteiner, WelcomeStyled } from './WelcomeStyled.styled.js';
 import { WhyDrinkWater } from './WhyDrinkWater/WhyDrinkWater.jsx';
+
+import { Conteiner, WelcomeStyled } from './WelcomeStyled.styled.js';
 
 const Welcome = () => {
   return (
