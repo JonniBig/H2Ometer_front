b67b1334301841d018fe23d2e5fbcd18787eb99d
bober2005 <yaroslavd1290@gmail.com>
2024-02-01 14:41:42 +0200

    setting

---
+++ src/components/Form/InputError.styled.jsx
@@ -0,0 +1,8 @@
+import theme from 'components/Setting/jsx';
+import styled from 'styled-components';
+
+export const InputError = styled.p`
+  font-size: 14px;
+  line-height: calc(18 / 14);
+  color: ${theme.colors.secondaryRed};
+`;
