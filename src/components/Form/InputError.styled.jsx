45425d5b10a89eed7488ffb2e677ef27b6bd2001
JonniBig <126974403+JonniBig@users.noreply.github.com>
2024-02-01 16:35:08 +0200

    Revert "setting"

--- src/components/Form/InputError.styled.jsx
+++
@@ -1,8 +0,0 @@
-import theme from 'components/Setting/jsx';
-import styled from 'styled-components';
-
-export const InputError = styled.p`
-  font-size: 14px;
-  line-height: calc(18 / 14);
-  color: ${theme.colors.secondaryRed};
-`;
