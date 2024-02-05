90686bcfe611c486557f4777b62e8e69d793d8ea
JonniBig <bigun.i88@gmail.com>
2024-02-05 21:00:38 +0200

    update

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import closeModal from '../../assets/images/icons/close-x.svg';
 import { Conteiner, DeleteStyled } from './DeleteModalStyled.styled';
 import { useSelector } from 'react-redux';
