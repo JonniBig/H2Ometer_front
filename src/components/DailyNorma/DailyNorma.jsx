e3aeca79b8824c504a4187d10d2f01b2babb1a86
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2024-02-07 00:51:09 +0100

    translate

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -3,11 +3,13 @@ import { StyledDailyNorma } from './DailyNorma.styled';
 import { FormDailyNorma, GeneralModal } from 'components';
 import { useSelector } from 'react-redux';
 import { selectWaterData } from '../../redux/calendar/calendarSlice.selectors';
+import { useTranslation } from 'react-i18next';
 
 const DailyNorma = () => {
   const [showModal, setShowModal] = useState(false);
   const isDarkMode = useSelector(state => state.theme.isDarkMode);
   const waterData = useSelector(selectWaterData);
+  const { t } = useTranslation();
 
   const toggleModal = () => {
     setShowModal(!showModal);
@@ -17,7 +19,7 @@ const DailyNorma = () => {
     <StyledDailyNorma
       className={`secondary-blue ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
     >
-      <p className="title">My daily norma</p>
+      <p className="title">{t('dailyNorm.title')}</p>
       <div className="content">
         <p className="volume">{waterData?.dailyNorma ?? '2'} L</p>
         <button className="edit" onClick={toggleModal}>
@@ -26,13 +28,14 @@ const DailyNorma = () => {
               isDarkMode ? 'dark-mode' : 'light-mode'
             }`}
           >
-            Edit
+            {t('dailyNorm.edit')}
           </span>
         </button>
       </div>
+
       {showModal && (
         <GeneralModal
-          title="My daily norma"
+          title={t('dailyNorm.title')}
           className="formDailiNorma"
           onClose={toggleModal}
           renderContent={onClose => <FormDailyNorma onClose={onClose} />}
--- src/components/Welcome/WhyDrinkWater/WhyDrinkWater.jsx
+++ src/components/Welcome/WhyDrinkWater/WhyDrinkWater.jsx
@@ -1,22 +1,24 @@
 import { useSelector } from 'react-redux';
 import { WhyDrinkWaterStyled } from './WhyDrinkWaterStyled.styled';
+import { useTranslation } from 'react-i18next';
 
 export const WhyDrinkWater = () => {
   const isDarkMode = useSelector(state => state.theme.isDarkMode);
+  const { t } = useTranslation();
 
   return (
     <WhyDrinkWaterStyled
       className={`dark-modal ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
     >
-      <h2>Why Drink Water</h2>
+      <h2>{t('whyDrinkWater.title')}</h2>
       <ul>
-        <li>Supply of nutrients to all organs</li>
-        <li>Providing oxygen to the lungs</li>
-        <li>Maintaining the work of the heart</li>
-        <li>Release of processed substances</li>
-        <li>Ensuring the stability of the internal environment</li>
-        <li>Maintaining within the normal temperature</li>
-        <li>Maintaining an immune system capable of resisting disease</li>
+        <li>{t('whyDrinkWater.list.item1')}</li>
+        <li>{t('whyDrinkWater.list.item2')}</li>
+        <li>{t('whyDrinkWater.list.item3')}</li>
+        <li>{t('whyDrinkWater.list.item4')}</li>
+        <li>{t('whyDrinkWater.list.item5')}</li>
+        <li>{t('whyDrinkWater.list.item6')}</li>
+        <li>{t('whyDrinkWater.list.item7')}</li>
       </ul>
     </WhyDrinkWaterStyled>
   );
